db356dd175b03489e478d4a36d881255624ad9fc
Kseniya Makarova <ksenija.mackarova2014@yandex.ru>
2022-03-30 18:22:45 +0300

    Добавьте файлы проекта.

---
+++ EventClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sobytiya
+{
+    public class EventClass
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+         
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                char symbol = key.KeyChar;
+                if (symbol == 'c')
+                    break;
+                else
+                {
+                    OnKeyPressed.Invoke(this, symbol);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace sobytiya
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var eventClass = new EventClass();
+            eventClass.OnKeyPressed += OutPutChar;
+            eventClass.Run();
+        }
+        public static void OutPutChar(object sender, char symbol)
+        {
+            Console.WriteLine(symbol);
+        }
+    }
+}
